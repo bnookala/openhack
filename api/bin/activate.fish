ffec7eb4e931f10f92a24722367c519562a202d7
Evan Louie <evan.louie@microsoft.com>
2017-11-08 13:45:00 -0500

    VirtualEnv fix

--- api/bin/activate.fish
+++ api/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/evlouie/Workspace/OpenHack/API"
+set -gx VIRTUAL_ENV "/Users/evlouie/Workspace/openhack/api"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
